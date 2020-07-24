b683a13deaa8575638740319e4c0b5b19cd99c6b
KarasuCvs <40797849+KarasuCvs@users.noreply.github.com>
2020-07-24 22:43:38 +0900

    3d测试 base

--- myslam_master/main.py
+++ myslam_master/main.py
@@ -10,8 +10,8 @@ import math
 
 t = 5
 Ts = 0.1
-x_dimension = 2
-y_dimension = 1
+x_dimension = 3
+y_dimension = 3
 N = int(t/Ts)
 time_line = np.linspace(0, t, N)
 
@@ -19,26 +19,25 @@ X_real = np.asmatrix(np.zeros((x_dimension, N)))
 sensor = np.asmatrix(np.zeros((y_dimension, N)))
 RealOutput = np.asmatrix(np.zeros((y_dimension, N)))
 MCKFObservation = np.zeros(N)
-noise_q = 1             # 系统噪音
+noise_q = 0             # 系统噪音
 noise_r = np.matrix([2])
 noise_state = np.asmatrix(np.sqrt(noise_q) * randn(x_dimension, N))
 noise_observation = np.asmatrix(int(np.sqrt(noise_r)) * randn(y_dimension, N))
 
-A = np.matrix([0, 1, 0, 0]).reshape(x_dimension, x_dimension)
-B = np.matrix([0, 1]).reshape(x_dimension, y_dimension)
-C = np.matrix([1, 0]).reshape(y_dimension, x_dimension)
+A = np.matrix([1, 0, 0, 0, 1, 0, 0, 0, 1]).reshape(x_dimension, x_dimension)
+B = np.matrix([1, 0, 0, 0, 1, 0, 0, 0, 1]).reshape(x_dimension, y_dimension)
+C = np.matrix([1, 0, 0, 0, 1, 0, 0, 0, 1]).reshape(y_dimension, x_dimension)
 
 input = np.asmatrix(np.ones((y_dimension, N)))
-# r = 1
-# theta = 10
+step = 1
+theta = 5
 for i in range(N):
-    # input[0, i] = input[0, i-1] + r*math.cos(theta)
-    # input[1, i] = input[1, i-1] + r*math.sin(theta)
-    # input[2, i] = input[2, i-1] + theta
-    input[0, i] = 1
+    input[2, i] = theta+input[2, i-1]
+    input[0, i] = step*math.cos(input[2, i]*math.pi/180)
+    input[1, i] = step*math.sin(input[2, i]*math.pi/180)
 for i in range(N):
     if i == 0:
-        X_real[:, 0] = np.matrix(([2], [1]))
+        X_real[:, 0] = np.matrix(([2], [1], [0]))
     else:
         # 欧拉法
         # X_real[:, i] = (A*X_real[:, i-1] + B*input[:, i-1])*Ts + X_real[:, i-1] + randn(x_dimension, 1)*np.sqrt(noise_q)
@@ -47,11 +46,14 @@ for i in range(N):
         #     (Ts/2*(A*X_real[:, i-1] + B*(input[:, i-1]+input[:, i]) +
         #      (randn(x_dimension, 1)*np.sqrt(noise_q)+randn(x_dimension, 1)*np.sqrt(noise_q))) + X_real[:, i-1])
         # 改进欧拉法
-        X_temp = (A*X_real[:, i-1] + B*input[:, i-1])*Ts + X_real[:, i-1]
-        X_real[:, i] = (A*X_real[:, i-1] + B*input[:, i-1] + X_temp + noise_state[:, i-1]+noise_state[:, i])*Ts/2 + X_real[:, i-1]
+        # X_temp = (A*X_real[:, i-1] + B*input[:, i-1])*Ts + X_real[:, i-1]
+        # X_real[:, i] = (A*X_real[:, i-1] + B*input[:, i-1] + X_temp + noise_state[:, i-1]+noise_state[:, i])*Ts/2 + X_real[:, i-1]
+        # 步进
+        X_real[:, i] = A*X_real[:, i-1] + B*input[:, i-1] + noise_state[:, i]
     sensor[:, i] = C*X_real[:, i] + noise_observation[:, i]
     RealOutput[:, i] = C*X_real[:, i]
 
+# Calculation
 mckf = MCKF()
 mckf.read_data(x_dimension, y_dimension, sensor)
 mckf.parmet(3, 0.2, noise_q, noise_r)
@@ -80,22 +82,13 @@ kf_mean = np.mean(kf_error)
 kf_std = np.std(kf_error)
 kf_pdf = norm.pdf(time_line-t/2, 0, kf_std)
 
-plt.subplot(221)
-plt.plot(np.linspace(0, t, N), np.array(sensor).reshape(N,), linewidth=1, linestyle="-", label="Sensor")
-plt.plot(np.linspace(0, t, N), np.array(MCKFObservation).reshape(N,), linewidth=1, linestyle="-", label="MCKF")
-plt.plot(np.linspace(0, t, N), np.array(KFObservation).reshape(N,), linewidth=1, linestyle="-", label="KF ")
-plt.plot(np.linspace(0, t, N), np.array(RealOutput).reshape(N,), linewidth=1, linestyle="-", label="Real State")
+
+plt.plot(np.array(sensor.T[:, 0]), np.array(sensor.T[:, 1]), linewidth=1, linestyle="-", label="Sensor")
+plt.plot(np.array(MCKFObservation.T[:, 0]), np.array(MCKFObservation.T[:, 1]), linewidth=1, linestyle="-", label="MCKF")
+plt.plot(np.array(KFObservation.T[:, 0]), np.array(KFObservation.T[:, 1]), linewidth=1, linestyle="-", label="KF ")
+plt.plot(np.array(RealOutput.T[:, 0]), np.array(RealOutput.T[:, 1]), linewidth=1, linestyle="-", label="Real State")
 plt.grid(True)
 plt.legend(loc='upper left')
-
-# plt.subplot(221)
-# plt.plot(sensor.T[:, 0], sensor.T[:, 0], linewidth=1, linestyle=".", label="Sensor")
-# plt.plot(MCKFObservation[0, :], MCKFObservation[1, :], linewidth=1, linestyle="-", label="MCKF")
-# plt.plot(np.linspace(0, t, N), np.array(KFObservation).reshape(N,), linewidth=1, linestyle="-", label="KF ")
-# plt.plot(RealOutput.T[:, 0], RealOutput.T[:, 0], linewidth=1, linestyle=".", label="Real State")
-# plt.scatter(np.array(RealOutput[0, :]), np.array(RealOutput[1, :]), label="Real State")
-# plt.grid(True)
-# plt.legend(loc='upper left')
 
 # plt.subplot(222)
 # plt.plot(time_line-t/2, sensor_pdf.reshape(N,), label="Sensor")
