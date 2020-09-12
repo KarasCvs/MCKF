Still something wrong, consider rebuild calculation part.

MCKF/functions/nonlinear_func.py
@@ -21,10 +21,23 @@ import numpy as np
 #     return observation
 
 # Nonlinear func NO.2, from paper[Maximum correntropy unscented filter]
+def state_func(states, k=0, Ts=0.1):
+    states_ = np.zeros(3).reshape(3, 1)
+    states_[0] = states[0] + Ts*states[1]
+    states_[1] = states[1] + Ts*(2*math.exp(-states[0]/2e4) * states[1] * states[1] * states[2]/2 - 32.2)
+    states_[2] = states[2]
+    return states_
+
+
+def observation_func(states, k=0, Ts=0.1):
+    observation = math.sqrt(1e5*1e5 + pow((states[0]-1e5), 2))
+    return observation
+
+# No.3
 # def state_func(states, k=0, Ts=0.1):
 #     states_ = np.zeros(3).reshape(3, 1)
-#     states_[0] = states[0] + Ts*states[1]
-#     states_[1] = states[1] + Ts*math.exp(-states[0]*5e-5) * pow(states[1], 2)*states[2] - Ts*32.2
+#     states_[0] = states[0] + Ts*(-states[1])
+#     states_[1] = states[1] + Ts * (-math.exp(-states[0]*5e-5) * pow(states[1], 2)*states[2])
 #     states_[2] = states[2]
 #     return states_
 
@@ -32,18 +45,6 @@ import numpy as np
 # def observation_func(states, k=0, Ts=0.1):
 #     observation = math.sqrt(pow(1e5, 2) + pow((states[0]-1e5), 2))
 #     return observation
-
-# No.3
-def state_func(states, k=0, Ts=0.1):
-    states_ = np.zeros(3).reshape(3, 1)
-    states_[0] = states[0] + Ts*(-states[1])
-    states_[1] = states[1] + Ts * (-math.exp(-states[0]*5e-5) * pow(states[1], 2)*states[2])
-    states_[2] = states[2]
-    return states_
-
-def observation_func(states, k=0, Ts=0.1):
-    observation = math.sqrt(pow(1e5, 2) + pow((states[0]-1e5), 2))
-    return observation
 
 
 if __name__ == "__main__":

MCKF/ukf/ukf_2.py
@@ -22,13 +22,11 @@ class UKF():
         self.ki = ki
         self.lambda_ = self.alpha*self.alpha*(self.states_dimension+self.ki) - self.states_dimension
         self.c_ = self.lambda_ + self.states_dimension                                      # scaling factor
-        self.W_mean = (np.hstack(((np.matrix(self.lambda_/self.c_)),
-                       0.5/self.c_+np.zeros((1, 2*self.states_dimension))))).A.reshape(self.states_dimension*2+1,)
+        self.W_mean = ((1-self.alpha)/2*self.states_dimension+np.zeros((1, 2*self.states_dimension+1))).reshape(self.states_dimension*2+1,)
         self.W_cov = self.W_mean
-        self.W_cov[0] = self.W_mean[0] + (1 - self.alpha*self.alpha + self.beta)                # only the first one is different
 
     def sigma_points(self, x_prior, P):
-        sigma_A_ = np.linalg.cholesky((self.c_) * P)
+        sigma_A_ = np.linalg.cholesky((self.states_dimension/(1-self.alpha)) * P)
         sigma_X_ = x_prior * np.ones((1, self.states_dimension))
         X_sigmas = np.hstack((x_prior, sigma_X_+sigma_A_, sigma_X_-sigma_A_))
         return X_sigmas