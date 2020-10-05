Fix calculation of L, build find() methode with recursion.

MCKF/gptest.py
@@ -28,7 +28,7 @@ gp.fit(train_x, train_y)
 y_pred, sigma = gp.predict(timeline, return_std=True)
 
 plt.figure()
-plt.plot(timeline, states, label='real')
+plt.plot(timeline, states, label='real', color='r')
 plt.plot(timeline, y_pred, 'b-', label=u'Prediction')
 # plt.fill(np.concatenate([timeline, timeline[::-1]]),
 #          np.concatenate([y_pred - 1.9600 * sigma,

MCKF/simulation.py
@@ -23,11 +23,11 @@ class Simulation():
         self.q = 0
         self.r = 20
         # additional noise
-        self.additional_noise = 20*np.random.randn(self.obs_dimension, int(self.t/self.Ts))
+        self.additional_noise = np.zeros((self.obs_dimension, int(self.t/self.Ts)))  # 20*np.random.randn(self.obs_dimension, int(self.t/self.Ts))
         for i in range(self.additional_noise.shape[1]):
-            if np.random.randint(0, 100) < 10:
-                self.additional_noise[:, i] = self.additional_noise[:, i]*np.random.randint(70, 90)
-        self.additional_noise = np.zeros((self.obs_dimension, int(self.t/self.Ts)))
+            if np.random.randint(0, 100) < 30:
+                self.additional_noise[:, i] = np.ones((self.obs_dimension, 1)) * np.random.randint(70, 90)
+        # self.additional_noise = np.zeros((self.obs_dimension, int(self.t/self.Ts)))
 
     def sys_run(self):
         # System initial